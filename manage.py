dc5f0281362d1f1c9be889974acf16f4ac0a127c
Gelzieny R. Martins <gelzieny@gmail.com>
2025-01-23 15:36:00 -0300

    Configurando projeto

---
+++ manage.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+  title="FastAPI do Zero",
+  version="0.1.0",
+  description="API criada com FastAPI para fins didáticos"
+)
+
+@app.get("/")
+async def root():
+  return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+  import uvicorn
+  uvicorn.run("manage:app", host="0.0.0.0", port=5092, log_level="info", reload=True)
